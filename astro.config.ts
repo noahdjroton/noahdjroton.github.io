cb2f1153c2a0d96ede1c8d6e00172e1e4bb31e57
Djroton Noé Sossou <70499809+noahdjroton@users.noreply.github.com>
2023-09-14 20:04:20 +0100

    Config github workflow

---
+++ astro.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
+
+// https://astro.build/config
+export default defineConfig({
+	// ! Please remember to replace the following site property with your own domain
+	site: "https://noahdjroton.github.io",
+	markdown: {
+		shikiConfig: {
+			theme: "dracula",
+			wrap: true,
+		},
+	},
+	integrations: [
+		mdx({}),
+		tailwind({
+			config: {
+				applyBaseStyles: false,
+			},
+		}),
+		image({
+			serviceEntryPoint: "@astrojs/image/sharp",
+		}),
+		sitemap(),
+		prefetch(),
+	],
+	compressHTML: true,
+	vite: {
+		optimizeDeps: {
+			exclude: ["@resvg/resvg-js"],
+		},
+	},
+});
